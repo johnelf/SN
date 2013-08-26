d4f1068d6e97caa0cc55e571a53c94b607b72700
Zhang Jinwen <john_elf@126.com>
2013-08-26 11:33:43 +0800

    Init SN

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SN::Application.config.secret_key_base = 'f419db1b73cbd8c54637f59f2369ad78a9f0709ef5802904c4f3cc82665388ae795fe9c9659490b8ed7fc3a06315852810d023ee2e1a8287a51164007fada01b'
