8fc739589dce09fd544a7c034c9bf046776ad47b
John <john_elf@126.com>
2013-09-11 17:25:07 +0800

    Revert "add routes"
    This reverts commit 4b08a8a9d824731259fa0cc06dcadb41e0e1849c.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ SN::Application.routes.draw do
   get "time_line/index"
   get "callback/index"
   get "welcome/index"
-  match '/welcome/index' => 'welcome#index'
-  match '/callback/index' => 'callback#index'
-
   root :to => "welcome#index"
 end
