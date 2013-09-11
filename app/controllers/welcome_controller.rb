8f2d75e70e5e9d2685cdaae4901503003998d150
John <john_elf@126.com>
2013-09-11 16:13:55 +0800

    make home page not redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     client = WeiboOAuth2::Client.new
 
-    redirect_to client.authorize_url
+    @auth_url = client.authorize_url
   end
 end
