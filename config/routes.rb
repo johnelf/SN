4b08a8a9d824731259fa0cc06dcadb41e0e1849c
John <john_elf@126.com>
2013-09-11 17:16:56 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ SN::Application.routes.draw do
   get "time_line/index"
   get "callback/index"
   get "welcome/index"
+  match '/welcome/index' => 'welcome#index'
+  match '/callback/index' => 'callback#index'
+
   root :to => "welcome#index"
 end
