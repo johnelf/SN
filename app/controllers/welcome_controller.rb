838a4a093c1abb28897ff55a60425e5668735bfa
Zhang Jinwen <john_elf@126.com>
2013-09-16 23:04:05 +0800

    remove old controllers

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    client = WeiboOAuth2::Client.new
-
-    @auth_url = client.authorize_url
-  end
-end
--- app/helpers/welcome_helper.rb
+++
@@ -1,7 +0,0 @@
-module WelcomeHelper
-  def reset_session
-    session[:uid] = nil
-    session[:access_token] = nil
-    session[:expires_at] = nil
-  end
-end
