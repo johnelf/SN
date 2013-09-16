74e6ed2314fb02a4826e7cd564dda2b947e10e99
John <john_elf@126.com>
2013-09-16 13:24:04 +0800

    make heroku work

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,9 +2,9 @@ class CallbackController < ApplicationController
   def index
     client = WeiboOAuth2::Client.new
     access_token = client.auth_code.get_token(params[:code].to_s)
-    session[:uid] = access_token.params["uid"]
-    session[:access_token] = access_token.token
-    session[:expires_at] = access_token.expires_at
+    #session[:uid] = access_token.params["uid"]
+    #session[:access_token] = access_token.token
+    #session[:expires_at] = access_token.expires_at
 
     #if session[:access_token] && !client.authorized?
     #  token = client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
