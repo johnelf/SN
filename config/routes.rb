d8075a1241b1442cea4829a08b631e567ce79884
jwzhang <john_elf@126.com>
2013-11-18 13:35:10 +0800

    fix bugs...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SN::Application.routes.draw do
 
   match "/" => "homepage#show", :via => [:get, :post]
-  match "/callback" => "homepage#callback", via => [:get, :post]
-  match "/connect" => "homepage#connect", via => :get
+  match "/callback" => "homepage#callback", :via => [:get, :post]
+  match "/connect" => "homepage#connect", :via => :get
 
   root 'homepage#show'
 end
