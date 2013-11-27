5a7bb2a24dbaca9f0624bdec4323a68b0ccae0f8
jwzhang <john_elf@126.com>
2013-11-27 09:59:52 +0800

    make homepage fancy

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,19 +3,21 @@ class HomepageController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def show
-    client = WeiboOAuth2::Client.new
-    if session[:access_token] && !client.authorized?
-      token = client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
+    @client = WeiboOAuth2::Client.new
+    if session[:access_token] && !@client.authorized?
+      @token = @client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
 
-      unless token.validated?
+      unless @token.validated?
         reset_session
         redirect '/connect'
         return
       end
     end
+
     if session[:uid]
-      @user = client.users.show_by_uid(session[:uid])
-      @statuses = client.statuses
+      debugger
+      @user = @client.users.show_by_uid(session[:uid])
+      @statuses = @client.statuses
     end
   end
 
