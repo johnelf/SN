7b5ce855c5451103e6845416cfd284e83e6e305c
jwzhang <john_elf@126.com>
2013-11-29 10:28:10 +0800

    add response code

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -3,12 +3,14 @@ class CallbackController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def callback
-
-    puts "callback............."
     client = WeiboOAuth2::Client.new
 
     code = params[:code]
     access_token = client.auth_code.get_token(code.to_s)
+
+    puts "============================="
+    puts "access_token: " + access_token
+    puts "============================="
 
     session[:uid] = access_token.params["uid"]
     session[:access_token] = access_token.token
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,22 +4,14 @@ class HomepageController < ApplicationController
 
   def show
     @client = WeiboOAuth2::Client.new
-    if session[:access_token] && !@client.authorized?
-          return
-      #@token = @client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
-      #
-      #unless @token.validated?
-      #  reset_session
-      #  redirect_to '/connect'
-      #  return
-      #end
-    end
 
     if session[:uid]
       puts "user info...................."
-      #@user = @client.users.show_by_uid(session[:uid])
       @statuses = @client.statuses
+      render :text => "homepage/success", :status => 201
     end
+
+    render :file => "homepage/show", :status => 200
   end
 
 end
