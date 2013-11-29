aa1fab12fdb8f0c0f00339a03fa0be0019b1a274
jwzhang <john_elf@126.com>
2013-11-29 09:54:17 +0800

    make it work

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,19 +1,9 @@
 class HomepageController < ApplicationController
-  protect_from_forgery :except => :index
-  skip_before_filter :verify_authenticity_token
+  #protect_from_forgery :except => :index
+  #skip_before_filter :verify_authenticity_token
 
   def show
     @client = WeiboOAuth2::Client.new
-    #if session[:access_token] && !@client.authorized?
-    #
-    #  @token = @client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
-    #
-    #  unless @token.validated?
-    #    reset_session
-    #    redirect_to '/connect'
-    #    return
-    #  end
-    #end
 
     if session[:uid]
       puts "user info...................."
@@ -21,11 +11,5 @@ class HomepageController < ApplicationController
       @statuses = @client.statuses
     end
   end
-
-  #def connect
-  #  client = WeiboOAuth2::Client.new
-  #
-  #  redirect client.authorize_url
-  #end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ SN::Application.routes.draw do
 
   match "/" => "homepage#show", :via => :get
   match "/" => "callback#callback", :via => :post
-  #match "/connect" => "homepage#connect", :via => :get
 
   root 'homepage#show'
 end
