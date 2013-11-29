354ffcaf4d9335b780776b06393235344406ce1b
jwzhang <john_elf@126.com>
2013-11-29 10:07:31 +0800

    work

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,19 @@
 class HomepageController < ApplicationController
-  #protect_from_forgery :except => :index
-  #skip_before_filter :verify_authenticity_token
+  protect_from_forgery :except => :index
+  skip_before_filter :verify_authenticity_token
 
   def show
     @client = WeiboOAuth2::Client.new
+    if session[:access_token] && !@client.authorized?
+          return
+      #@token = @client.get_token_from_hash({:access_token => session[:access_token], :expires_at => session[:expires_at]})
+      #
+      #unless @token.validated?
+      #  reset_session
+      #  redirect_to '/connect'
+      #  return
+      #end
+    end
 
     if session[:uid]
       puts "user info...................."
