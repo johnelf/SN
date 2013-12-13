8edbf69d6959ea677384c7cb455a4d01d95a6ca1
jwzhang <john_elf@126.com>
2013-12-13 10:44:31 +0800

    spike for d3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SN::Application.routes.draw do
 
   match "/" => "homepage#show", :via => :get
   match "/" => "callback#callback", :via => :post
+  match "/test" => "test#test", :via => :get
 
   root 'homepage#show'
 end
